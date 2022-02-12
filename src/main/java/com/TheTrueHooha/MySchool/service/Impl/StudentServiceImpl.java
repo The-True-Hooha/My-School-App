aa956dc5a644da9cad76c494e5ab9e9b65b751f2
David Ogar <owogogahhero@outlook.com>
2022-02-12 11:48:07 +0100

    created a new method and implemented to get all students

--- src/main/java/com/TheTrueHooha/MySchool/service/Impl/StudentServiceImpl.java
+++ src/main/java/com/TheTrueHooha/MySchool/service/Impl/StudentServiceImpl.java
@@ -1,8 +1,28 @@
 package com.TheTrueHooha.MySchool.service.Impl;
 //implementation class that implements that StudentService class
 
+import com.TheTrueHooha.MySchool.entity.Student;
+import com.TheTrueHooha.MySchool.repository.StudentRepository;
 import com.TheTrueHooha.MySchool.service.StudentService;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+//implements the method to get all students
 public class StudentServiceImpl implements StudentService {
 
+    private StudentRepository studentRepository;
+
+    //constructor
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<Student> getAllStudent() {
+
+        return studentRepository.findAll();
+    }
 }
--- src/main/java/com/TheTrueHooha/MySchool/service/StudentService.java
+++ src/main/java/com/TheTrueHooha/MySchool/service/StudentService.java
@@ -1,4 +1,11 @@
 package com.TheTrueHooha.MySchool.service;
 
+import com.TheTrueHooha.MySchool.entity.Student;
+
+import java.util.List;
+
 public interface StudentService {
+
+    //defines a method that gets al students
+    List <Student> getAllStudent ();
 }
