17d3746d7c3d46d27c8e8f5149f38ae95bb1e332
David Ogar <owogogahhero@outlook.com>
2022-02-12 17:46:40 +0100

    completed the get request methods

--- src/main/java/com/TheTrueHooha/MySchool/MySchoolApplication.java
+++ src/main/java/com/TheTrueHooha/MySchool/MySchoolApplication.java
@@ -1,13 +1,45 @@
 package com.TheTrueHooha.MySchool;
 
+import com.TheTrueHooha.MySchool.entity.Student;
+import com.TheTrueHooha.MySchool.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class MySchoolApplication {
+public class MySchoolApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MySchoolApplication.class, args);
 	}
 
+	@Autowired
+	private StudentRepository studentRepository ;
+
+	@Override
+	public void run(String... args) throws Exception {
+		//parsing student data that gets save into the student repo object
+
+		Student student1 = new Student("Kings", "Bright", "brightog@gmail.com",
+				"male", "02/2/2022", "22 abiola stree");
+		studentRepository.save(student1);
+
+		Student student2 = new Student("Jadon", "praise", "jp@gmail.com",
+				"male", "19/5/1992", "22 bright street");
+		studentRepository.save(student2);
+
+		Student student3 = new Student("Vicky", "praise", "jp@gmail.com",
+				"female", "19/5/1992", "22 bright street");
+		studentRepository.save(student3);
+
+		Student student4 = new Student("Agnes", "praise", "jp@gmail.com",
+				"female", "19/5/1992", "22 bright street");
+		studentRepository.save(student4);
+
+		Student student5 = new Student("Grace", "praise", "jp@gmail.com",
+				"female", "19/5/1992", "22 bright street");
+		studentRepository.save(student5);
+	}
 }
--- src/main/java/com/TheTrueHooha/MySchool/controller/StudentController.java
+++ src/main/java/com/TheTrueHooha/MySchool/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.TheTrueHooha.MySchool.controller;
 
+import com.TheTrueHooha.MySchool.entity.Student;
 import com.TheTrueHooha.MySchool.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class StudentController {
@@ -16,10 +18,20 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    //a request method to handle students request
+    //a get method to handle students request
     @GetMapping ("/students")
-    public String listStudent ( Model model) {
+    public String listStudents ( Model model) {
         model.addAttribute("students", studentService.getAllStudent());
         return "students";
+    }
+
+    //post method to handle creating of new students
+    @PostMapping ("/students/create-new")
+    public String createStudent (Model model) {
+
+        //creates a new student object to hold student data
+        Student student = new Student();
+        model.addAttribute("student", student);
+        return "create-new";
     }
 }
