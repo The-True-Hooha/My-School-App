ea6b1b78f85bdaba5e73705455e1781e610b1f58
David Ogar <owogogahhero@outlook.com>
2022-02-12 11:57:41 +0100

    initializing bootstrap

--- src/main/java/com/TheTrueHooha/MySchool/controller/StudentController.java
+++ src/main/java/com/TheTrueHooha/MySchool/controller/StudentController.java
@@ -1,9 +1,25 @@
 package com.TheTrueHooha.MySchool.controller;
 
+import com.TheTrueHooha.MySchool.service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class StudentController {
 
+    private StudentService studentService;
 
+    //a constructor for the student service controller
+    public StudentController(StudentService studentService) {
+        super();
+        this.studentService = studentService;
+    }
+
+    //a request method to handle students request
+    @GetMapping ("/students")
+    public String listStudent ( Model model) {
+        model.addAttribute("students", studentService.getAllStudent());
+        return "students";
+    }
 }
